d3099d20a2bc76f2f973a30ac85558f5965f2820
lukeNorris1 <lukenorris2@gmail.com>
2023-05-29 06:44:37 +1000

    Added component for images and they not fit to size

--- src/components/ImageView.tsx
+++ src/components/ImageView.tsx
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import redditApiImageGetter from "reddit-api-image-getter";
 
 export default function ImageView() {
+  const [imgHolder, setImgHolder] = useState(null);
+
+  useEffect(() => {
+    console.log(imgHolder);
+  }, [imgHolder]);
+
+  useEffect(() => {
+    let getter = new redditApiImageGetter();
+
+    getter
+      .getHotImagesOfSubReddit("ProgrammerHumor")
+      .then(function (result) {
+        setImgHolder(result.filter((index) => index.imageUrl != undefined));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <>
-    </>
+    <div className="grid grid-cols-5 grid-rows-5 h-screen">
+      {imgHolder != null
+        ? imgHolder.map((imageObject, index) => {
+            return (
+              <div key={index}>
+                <a href={imageObject.url}>
+                  {imageObject.imageUrl != undefined ? (
+                    <img
+                      className="object-cover h-full w-full"
+                      src={imageObject.imageUrl}
+                    />
+                  ) : (
+                    <></>
+                  )}
+                </a>
+              </div>
+            );
+          })
+        : null}
+      <div></div>
+    </div>
   );
 }
